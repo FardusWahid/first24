05623c042279e7a284e2a9725c61e60f5bf30ce4
Fardus wahid <farduswahid87@gmail.com>
2024-04-27 19:34:36 +0600

    style

--- src/app/freecourses/frb/page.jsx
+++ src/app/freecourses/frb/page.jsx
@@ -5,60 +5,60 @@ import { GrDocumentPdf } from "react-icons/gr";
 import { GrNext } from "react-icons/gr";
 export default function Ict(){
     return(
-<div className="bg-gradient-to-r from-[#000000] to-[#10012b]">
+<div>
            <Navbar/>
             <div>
-<h2 className="text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-white via-yellow-100 to-blue-400 text-[33px] md:text-5xl">Final Revision 2024 </h2>
+<h2 className="text-center font-sans font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-600 via-sky-200 to-blue-900 text-[33px] md:text-5xl">Final Revision 2024 </h2>
 
 
-<section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-3 justify-center
-bg-gradient-to-r from-[#44beffa8] via-[#6c3066] to-[#02236d] shadow-lg ring-1 hover:ring-yellow-400 rounded-2xl transition ease-in duration-300">
-         <a className="flex gap-4  hover:text-blue-400 active:text-purple-800 truncate"
-          href="/freecourses/frb/math"><h1 className="text-lg font-extrabold">Mathematics 1st</h1> <GrNext size={25} /></a>
+<section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-2 justify-center
+bg-gradient-to-r from-[#000102a8] to-[#230442a5] shadow-inner shadow-gray-700 ring-1 hover:ring-yellow-400 hover:tracking-wider rounded-lg transition ease-in duration-300">
+         <a className="flex gap-4  hover:text-yellow-200 active:text-yellow-200 active:tracking-widest"
+          href="/freecourses/frb/math"><h1 className="font-mono font-extrabold">Mathematics 1st</h1> <GrNext size={25} /></a>
        </section>
 
 
-       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-3 justify-center
-bg-gradient-to-r from-[#0aeb7e] via-[#d97b08ac] to-[#b6013d] shadow-lg ring-1 hover:ring-yellow-400 rounded-2xl transition ease-in duration-300">
-         <a className="flex gap-4 text-gray-200 hover:text-purple-900 truncate"
-          href="/freecourses/frb/math2"><h1 className="text-lg font-extrabold ">Mathematics 2nd</h1> <GrNext size={25} /></a>
+       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-2 justify-center
+bg-gradient-to-r from-[#2b28586e] to-[#040001] shadow-inner shadow-blue-800 ring-1 hover:ring-yellow-400 hover:tracking-wider rounded-lg transition ease-in duration-300">
+         <a className="flex gap-4 text-gray-200 hover:text-yellow-200 active:text-yellow-200 active:tracking-widest"
+          href="/freecourses/frb/math2"><h1 className="font-mono font-extrabold ">Mathematics 2nd</h1> <GrNext size={25} /></a>
        </section>
 
 
-       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-3 justify-center
-bg-gradient-to-r from-[#830505] via-[#FF44EC] to-[#d0d070] shadow-lg ring-1 hover:ring-yellow-400 rounded-2xl transition ease-in duration-300">
-         <a className="flex gap-6 text-gray-200 hover:text-purple-900 truncate"
-          href="/freecourses/frb/phy/1st"><h1 className="text-lg font-extrabold ">Physics 1st</h1> <GrNext size={25} /></a>
+       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-2 justify-center
+bg-gradient-to-r from-[#420505]  to-[#1f0404] ring-1 hover:ring-yellow-400 hover:tracking-wider rounded-lg shadow-inner shadow-red-900 transition ease-in duration-300">
+         <a className="flex gap-6 text-gray-200 hover:text-yellow-200 active:text-yellow-200 active:tracking-widest"
+          href="/freecourses/frb/phy/1st"><h1 className="font-mono font-extrabold ">Physics 1st</h1> <GrNext size={25} /></a>
        </section>
 
-       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-3 justify-center
-bg-gradient-to-r from-[#2f3030] via-[#4551f7] to-[#73662a] shadow-lg ring-1 hover:ring-yellow-400 rounded-2xl transition ease-in duration-300">
-         <a className="flex gap-5 text-gray-200 hover:text-purple-900 truncate"
-          href="/freecourses/frb/phy/2nd"><h1 className="text-lg font-extrabold ">Physics 2nd</h1> <GrNext size={25} /></a>
+       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-2 justify-center
+bg-gradient-to-r from-[#06670b00]  to-[#5d062982] ring-1 hover:ring-yellow-400 hover:tracking-wider shadow-green-900 rounded-lg shadow-inner transition ease-in duration-300">
+         <a className="flex gap-5 text-gray-200 hover:text-yellow-200 active:text-yellow-200 active:tracking-widest"
+          href="/freecourses/frb/phy/2nd"><h1 className="font-mono font-extrabold ">Physics 2nd</h1> <GrNext size={25} /></a>
        </section>
 
-       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-3 justify-center
-bg-gradient-to-r from-[#44beff00] via-[#5a9a21] to-[#c20f0665] shadow-lg ring-1 hover:ring-yellow-400 rounded-2xl transition ease-in duration-300">
-         <a className="flex gap-5 text-gray-200 hover:text-purple-900 truncate"
-          href="/freecourses/frb/chem/1st"><h1 className="text-lg font-extrabold ">Chemistry 1st</h1> <GrNext size={25} /></a>
+       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-2 justify-center
+bg-gradient-to-r from-[#44beff00]  to-[#31080565] ring-1 hover:ring-yellow-400 hover:tracking-wider rounded-lg shadow-inner shadow-blue-700 transition ease-in duration-300">
+         <a className="flex gap-5 text-gray-200 hover:text-yellow-200 active:text-yellow-200 active:tracking-widest"
+          href="/freecourses/frb/chem/1st"><h1 className="font-mono font-extrabold ">Chemistry 1st</h1> <GrNext size={25} /></a>
        </section>
        
-       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-3 justify-center
-bg-gradient-to-r from-[#37006a6d] via-[#883d04] to-[#540602] shadow-lg ring-1 hover:ring-yellow-400 rounded-2xl transition ease-in duration-300">
-         <a className="flex gap-4 text-gray-200 hover:text-purple-900 truncate"
-          href="/freecourses/frb/chem/2nd"><h1 className="text-lg font-extrabold ">Chemistry 2nd</h1> <GrNext size={25} /></a>
+       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-2 justify-center
+bg-gradient-to-r from-[#060123]  to-[#060b07] ring-1 hover:ring-yellow-400 hover:tracking-wider rounded-lg shadow-inner shadow-gray-700 transition ease-in duration-300">
+         <a className="flex gap-4 text-gray-200 hover:text-yellow-200 active:text-yellow-200 active:tracking-widest"
+          href="/freecourses/frb/chem/2nd"><h1 className="font-mono font-extrabold ">Chemistry 2nd</h1> <GrNext size={25} /></a>
        </section>
 
-       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-3 justify-center
-bg-gradient-to-r from-[#b7ff01] via-[#000000] to-[#2f0069] shadow-lg ring-1 hover:ring-yellow-400 rounded-2xl transition ease-in duration-300">
-         <a className="flex gap-2 text-gray-200 hover:text-purple-900 truncate"
-          href="/freecourses/frb/e2nd"><h1 className="text-lg font-extrabold ">English 2nd paper</h1> <GrNext size={25} /></a>
+       <section className="w-9/12 mt-4 m-auto flex md:w-1/4 p-2 justify-center
+bg-gradient-to-r from-[#62ff0007] to-[#18072d] ring-1 hover:ring-yellow-400 hover:tracking-wider rounded-lg shadow-lg shadow-blue-950 transition ease-in duration-300">
+         <a className="flex gap-2 text-gray-200 hover:text-yellow-200 active:text-yellow-200 active:tracking-widest"
+          href="/freecourses/frb/e2nd"><h1 className="font-mono font-extrabold ">English 2nd paper</h1> <GrNext size={25} /></a>
        </section>
 
             </div>
             <br />
- <section className="w-4/5 m-auto lg:w-1/3">
- <p className="flex justify-center"> <a className="flex px-6 md:px-16 py-1 font-bold text-end rounded-xl text-gray-900 bg-gradient-to-r from-blue-500 via-orange-700 to-pink-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-lime-300 " href="https://tinyurl.com/frb244" target="_blank"> Lecture & Practice sheets &nbsp; <GrDocumentPdf size={22} />
+ <section className="w-9/12 m-auto md:w-1/3">
+ <p className="flex justify-center"> <a className="flex tracking-wide font-mono px-1 md:px-16 py-1 font-bold text-end rounded-md text-gray-900 bg-gradient-to-r from-blue-500 via-orange-700 to-pink-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-lime-300 " href="https://tinyurl.com/frb244" target="_blank"> Lecture & Practice sheets &nbsp; <GrDocumentPdf size={22} />
  </a></p> 
  </section>
  <br /><br /><br /><br />
