ca874f7ce42b57d6109d0f53fa014b8a5bc8008b
Fardus wahid <farduswahid87@gmail.com>
2024-05-19 02:55:36 +0600

    gv

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={metadata.description} />
       <meta name="keywords" content={metadata.keywords} />
+      <meta name="google-site-verification" content="5A6r0nl48WzkentBg3bhahhCZxhXM90Bl2_eQ6q9vzk" />
       <title>{metadata.title}</title>
     </head>
     
